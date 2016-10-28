cb278c4f1c2c637a12da5e186801e4b6196310c7
Chris Hitchcott <hitchcott@gmail.com>
2016-10-28 14:42:51 -0700

    add cli docs

--- src/bin/doxity.js
+++ src/bin/doxity.js
@@ -1,7 +1,35 @@
 #!/usr/bin/env node
+import fs from 'fs';
+import path from 'path';
 import minimist from 'minimist';
 import * as Doxity from '../index';
 
 const args = minimist(process.argv.slice(2));
 
-Doxity.default[args._[0]](args);
+// get json version
+
+if (!args._[0]) {
+  const { version } = JSON.parse(fs.readFileSync(path.join(__dirname, '../../package.json')).toString());
+  console.log(`
+Doxity v${version}
+
+Commands:
+
+init       Initialize your project for use with doxity
+compile    Compile solidity contracts to generate docs data
+develop    Spin up a development server for customizing output
+publish    Generate static HTML documenation
+build      compile + publish
+
+Parameters:
+
+--target   Gatsby project source files directory
+--src      Folder that contains the contracts you want to compile
+--dir      Folder in gatsby project to dump contract data
+--out      Folder to output the generated html (relative to project root)
+--source   Git url for bootstrapping the gatsby project
+  `);
+  process.exit();
+} else {
+  Doxity.default[args._[0]](args);
+}
